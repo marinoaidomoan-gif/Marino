2e36ee7507b7ee3f6d10d168b45f06bfe56cf90d
marinoaidomoan <marinoaidomoan@gmail.com>
2026-08-17 17:39:36 +0100

    Ajout du css du header

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // ===== Footer : Année dynamique =====
-document.addEventListener('DOMContentLoaded', function() {
-    // 1. Année du copyright
-    const yearSpan = document.getElementById('annee-copyright');
-    if (yearSpan) {
-        yearSpan.textContent = new Date().getFullYear();
-    }
-});
+// Année dynamique
+const anneeCourante = document.getElementById('annee-courante');
+if (anneeCourante) {
+  anneeCourante.textContent = new Date().getFullYear();
+}
